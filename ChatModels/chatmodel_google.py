5f11da6d95611cb2076f8338fef95efdebb7854f
vankteshdixit <dixitvanktesh08@gmail.com>
2026-04-16 16:25:59 +0530

    just some checks

--- ChatModels/chatmodel_google.py
+++ ChatModels/chatmodel_google.py
@@ -12,8 +12,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# os.environ["GOOGLE_API_KEY"] = "AIzaSyA6ib7UMCnTbymkO915YMug4IaNpp8mQH8"
-
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
 response = model.invoke("Why do parrots talk?")
